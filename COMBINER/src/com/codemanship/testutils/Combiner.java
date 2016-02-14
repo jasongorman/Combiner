eed544c5425c8d2aff2930e5a1ec45305e765a6c
Jason Gorman <jason.gorman@codemanship.com>
2016-02-14 14:01:56 +0000

    Initial commit

---
+++ COMBINER/src/com/codemanship/testutils/Combiner.java
@@ -0,0 +1,58 @@
+package com.codemanship.testutils;
+
+public class Combiner {
+
+	private Object[][] combined;
+
+	public Object[][] combineArrays(Object[][] arrays) {
+		buildEmptyCombinedArray(arrays);		
+		populateCombinedArray(arrays);			
+		return combined;
+	}
+
+	public void populateCombinedArray(Object[][] arrays) {
+		for (int i = 0; i < arrays.length; i++) {
+			populateColumn(arrays, i);
+		}
+	}
+
+	private void populateColumn(Object[][] arrays, int columnIndex) {
+		int repetitions = calculateRepetitions(arrays, columnIndex);
+		insertColumnValues(columnIndex, repetitions, arrays[columnIndex]);
+	}
+
+	private void insertColumnValues(int columnIndex, int repetitions,
+			Object[] columnSource) {
+		for(int i = 0; i < combined.length; i += (repetitions * columnSource.length)){
+			for(int j = 0;j < columnSource.length;j++){ 
+				for(int k = 0;k < repetitions;k++){
+					combined[i + (j * repetitions) + k][columnIndex] = columnSource[j];
+				}
+			}
+		}
+	}
+
+	private int calculateRepetitions(Object[][] arrays, int columnIndex) {
+		int repetitions = 1;
+		for (int i = 0; i < arrays.length; i++) {
+			if(i > columnIndex){
+				repetitions *= arrays[i].length;
+			}
+		}
+		return repetitions;
+	}
+
+	public void buildEmptyCombinedArray(Object[][] arrays) {
+		int numberOfArrays = arrays.length;
+		int numberOfCombinations = 1;
+		for (Object[] array : arrays) {
+			numberOfCombinations *= array.length;
+		}
+		combined = new Object[numberOfCombinations][numberOfArrays];
+	}
+
+	public static Object[][] combine(Object[][] arrays) {
+		return new Combiner().combineArrays(arrays);
+	}
+
+}
---
+++ COMBINER/test/com/codemanship/testutils/CombinerTests.java
@@ -0,0 +1,62 @@
+package com.codemanship.testutils;
+import static com.codemanship.testutils.Combiner.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.codemanship.testutils.Combiner;
+
+public class CombinerTests {
+
+	@Test
+	public void singleArrayIsReturnedAsSingleCombination() {
+		assertArrayEquals(new Object[][]{{1}}, combine(new Object[][]{{1}}));
+	}
+	
+	@Test
+	public void twoArraysWithOneElementAndTwoElementsReturnedAsTwoCombinations() throws Exception {
+		assertArrayEquals(new Object[][]{{1,2},{1,3}}, 
+								combine(new Object[][]{{1},{2,3}}));
+	}
+	
+	@Test
+	public void twoArraysWithTwoElementsEachReturnedAsFourCombinations() throws Exception {
+		assertArrayEquals(new Object[][]{{1,3},{1,4},{2,3},{2,4}}, 
+								combine((new Object[][]{{1,2},{3,4}})));
+	}
+	
+	@Test
+	public void threeArraysReturnCombinationsWithThreeElements() throws Exception {
+		assertArrayEquals(new Object[][]{{1,2,3}}, 
+								combine(new Object[][]{{1},{2},{3}}));
+	}
+	
+	@Test
+	public void bigTomaleTest() throws Exception {
+		assertArrayEquals(new Object[][]{
+				{1,"A",true}, 
+				{1,"A",false},
+				{1,"B",true},
+				{1,"B",false},
+				{2,"A",true}, 
+				{2,"A",false},
+				{2,"B",true},
+				{2,"B",false}},
+				combine(new Object[][]{{1,2},{"A","B",},{true,false}}));
+	}
+	
+	@Test
+	public void anotherBigTest() throws Exception {
+		Object a = new Integer(1);
+		Object b = new Combiner();
+		
+		assertArrayEquals(new Object[][]{
+				{a,b,1,"A"},
+				{a,b,2,"A"},
+				{a,b,3,"A"},
+				{a,null,1,"A"},
+				{a,null,2,"A"},
+				{a,null,3,"A"}},		
+				combine(new Object[][]{{a},{b,null},{1,2,3},{"A"}}));
+	}
+}
